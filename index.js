f2e400dfe73eadfd2f956c0bbe6a01f64871e242
Jason Fernandes <jason.fernandes17@gmail.com>
2019-06-20 20:09:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,3 +35,10 @@ function removeLastKitten() {
 var cats = kittens.slice(0,kittens.length -1)
 return cats
 }
+
+function removeFirstKitten() {
+  var cats = kittens.slice(1)
+  return cats
+}
+
+}
